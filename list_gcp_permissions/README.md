69b28a5e270fa6fbbf99cdfe1f56048401a8606e
David Kim <davidkim@twitter.com>
2018-10-11 12:16:56 -0700

    adding multiprocessing because it's taking forever

--- list_gcp_permissions/README.md
+++ list_gcp_permissions/README.md
@@ -1,1 +1,3 @@
 Simple script that lists all of the permissions available by shelling out to gcloud
+
+python list_gcp_permissions.py --threads=20 --output=permissions.yaml
--- list_gcp_permissions/list_gcp_permissions.py
+++ list_gcp_permissions/list_gcp_permissions.py
@@ -1,4 +1,5 @@
 import argparse
+import multiprocessing
 import subprocess
 import yaml
 
@@ -18,27 +19,40 @@ def get_permissions(role):
     if role:
         try:
             output = subprocess.check_output("gcloud iam roles describe {}".format(role), shell=True) # noqa
-            return yaml.safe_load(output).get("includedPermissions", [])
+            return {"role": {"name": role, "permissions": yaml.safe_load(output).get("includedPermissions", [])}} # noqa
         except:
-            return []
-    return []
+            return {"role": {"name": role, "permissions": []}}
+    return {"role": {"name": role, "permissions": []}}
 
 
 def setup_cli():
     parser = argparse.ArgumentParser(description="Process some integers.")
     parser.add_argument("--output", type=str, help="output filename")
+    parser.add_argument("--threads", type=int, help="number of threads to use")
     args = parser.parse_args()
     return args
 
 
-if __name__ == "__main__":
-    roles = get_roles()
-    permissions_map = {}
-    for role in roles:
-        permissions_map["role"] = get_permissions(role)
-    args = setup_cli()
-    if args.output:
-        with open(args.output, "w") as f:
+def write_output(output_file, permissions_map):
+    if output_file:
+        with open(output_file, "w") as f:
             yaml.dump(permissions_map, f, default_flow_style=False)
     else:
         print(yaml.dump(permissions_map))
+
+
+if __name__ == "__main__":
+    args = setup_cli()
+    roles = get_roles()
+    permissions_map = {}
+
+    if args.threads:
+        thread_pool = multiprocessing.Pool(args.threads)
+        permissions_role_mappings = thread_pool.map(get_permissions, roles)
+        permissions_map["roles"] = permissions_role_mappings
+    else:
+        permissions_map["roles"] = []
+        for role in roles:
+            permissions_map["roles"].append(get_permissions(role))
+
+    write_output(args.output, permissions_map)
